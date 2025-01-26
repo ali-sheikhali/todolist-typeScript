c95f97ffb132c7ebe3aaf3c5cda42479537c26bd
ali-sheikhali <ali.sheikhali1998@gmail.com>
2025-01-26 16:27:38 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+// import "./App.css";
+
+function App() {
+  return (
+    <main className="main">
+      <p className="bg-red-500 text-yellow-200">hello world</p>
+    </main>
+  );
+}
+
+export default App;
